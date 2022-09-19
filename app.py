8c2edd717ed4082fecc8b4608e816699281eb18b
Vicranzer <victormaina1962@gmail.com>
2022-09-19 12:48:55 +0300

    model prediction and accuracy computation

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-from flask import Flask,render_template,request,url_for,redirect
+from flask import Flask,render_template,request,url_for,redirect,session,flash
 import numpy as np
 import xgboost as xgb
 import os
-from modelutils import predict,datapreprocessing,train,modelgeneration,computemetrics
+import pickle as pk
+from modelutils import ModelUtils as mutils
+# predict,datapreprocessing,train,modelgeneration,computemetrics
 from sendnotification import sendmail
+from datetime import timedelta
 app=Flask(__name__)
-diabetesdataurl='diabetes.csv'
+app.permanent_session_lifetime=timedelta(hours=3)
+app.secret_key='192b9bdd22ab9ed4d12e236c78afcb9a393ec15f71bbf5dc987d54727823bcbf'
 datasetpath='./static/Datasets'
-datasetfile= "diabetespredmodelusingxgboost.json"
+datasetfile= "diabetes.csv"
 datasetpathfile=os.path.join(datasetpath,datasetfile)
+modelspath='./static/ML Model'
+modelfile= "diabetespredmodelusingxgboost.pkl"
+modelpathfile=os.path.join(modelspath,modelfile)
 # Constructing web routes
 @app.route("/")
 def index():
@@ -16,16 +23,35 @@ def index():
 # Patients
 @app.route("/login",methods=["POST","GET"])
 def login():
-    return render_template('/patients/login.html')
+    if request.method=='POST':
+        session.permanent=True
+        user=request.form['username']
+        session['user']=user
+        flash(f'Login successful welcome {user}')
+        return redirect(url_for('dashboard'))
+    else: 
+        if 'user' in session:
+            flash('Already logged in')
+            return redirect(url_for('dashboard'))  
+        return render_template('/patients/login.html')
 @app.route("/logout")
 def logout():
+    if 'user' in session:
+        user=session['user']    
+        flash('You have been logged out!','info')
+    session.pop('user',None)
     return redirect(url_for("login"))
 @app.route("/register",methods=["POST","GET"])
 def register():
     return render_template('/patients/register.html')
 @app.route("/dashboard")
 def dashboard():
-    return render_template('/patients/dashboard.html')
+    if 'user' in session:
+        user=session['user']
+        return render_template('/patients/dashboard.html',user=user)
+    else:
+        flash('You are not logged in')
+        return redirect(url_for('login'))
 @app.route("/editinfo",methods=["POST","GET"])
 def editinfo():
     return render_template('/patients/editinfo.html')
@@ -61,7 +87,23 @@ def doctordashboard():
 @app.route("/doctors/predict", methods=['POST','GET'])
 def predict():
     if request.method=='POST':
-        predictionvalue=predict()
+        classifier=pk.load(open(modelpathfile,'rb'))
+        pregnanciesno=request.form["pregnanciesno"]
+        glucose=request.form["glucoselevels"]
+        height=request.form["height"]
+        weight=request.form["weight"]
+        bmi=float(weight)/(float(height)**2)
+        insulin=request.form["insulinlevels"]
+        Age=request.form["age"]
+        # bloodpressure=request.form["pressurelevels"]
+        pedigree=request.form["Pedigree"]
+        SkinThickness=request.form["skinthickness"]
+        features=[pregnanciesno,glucose,SkinThickness,insulin,bmi,pedigree,Age]
+        features=[float (x) for x in features]
+        # features=[6,148,72,35,0,33.6,50]
+        features=np.array(features)
+        features=features.reshape(1,-1)
+        predictionvalue=classifier.predict(features)
         if predictionvalue==1:
 
           return render_template('/doctors/predictdisease.html',pred="You have higher chances of Diabetes")
@@ -72,10 +114,14 @@ def predict():
         #   objectRep.close()
     else:
          return render_template('/doctors/predictdisease.html')
+@app.route("/doctors/train")
+def trainmodel():
+    X,Y=mutils.datapreprocessing(datasetpathfile)
+    return render_template('/doctors/accuracycomputation.html',metrics=mutils.train(X,Y))
 @app.route("/doctors/accuracy")
 def computeaccuracy():
-    X,Y=datapreprocessing(datasetpathfile)
-    return render_template('/doctors/accuracycomputation.html',metrics=computemetrics(X,Y))
+    X,Y=mutils.datapreprocessing(datasetpathfile)
+    return render_template('/doctors/accuracycomputation.html',metrics=mutils.computemetrics(X,Y))
 @app.route("/doctors/reports")
 def doctorsreports():
     return render_template('/doctors/reports.html')
--- modelutils.py
+++ modelutils.py
@@ -1,156 +1,103 @@
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import StandardScaler,MinMaxScaler
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import roc_auc_score, accuracy_score,f1_score,mean_absolute_error
-from sklearn.metrics import accuracy_score,f1_score,mean_squared_error,confusion_matrix,classification_report
-import xgboost as xgb
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import svm
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score,f1_score,mean_squared_error,confusion_matrix,classification_report
+
+import pickle as pk
 import os
-modelspath='./static/ML Model'
-modelfile= "diabetespredmodelusingxgboost.json"
-modelpathfile=os.path.join(modelspath,modelfile)
-def datapreprocessing(diabetesdataurl):
-    diabetesdata= pd.read_csv(diabetesdataurl)
-    diabetesdata[['Glucose','BloodPressure','SkinThickness','Insulin','BMI']] = diabetesdata[['Glucose','BloodPressure','SkinThickness','Insulin','BMI']].replace(0,np.NaN)
-    diabetesdata.dropna(inplace=True)
-    X=diabetesdata.drop(['Outcome'],axis=1)
-    Y=diabetesdata['Outcome']
-    scaler = StandardScaler()
-    # X = scaler.fit_transform(X)
-    return X,Y
-def train(X,Y):  
-    X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2,stratify=Y,random_state=42)
-    xgb_cls=xgb.XGBClassifier(learning_rate =0.0001,n_estimators=1000, max_depth=5,min_child_weight=1,gamma=0,subsample=0.8, colsample_bytree=0.8,objective= 'binary:logistic',nthread=4,scale_pos_weight=1,seed=27)
-    classifier=xgb_cls.fit(X_train,Y_train)
-    trainpreds=classifier.predict(X_train)
-    testpreds=classifier.predict(X_test)
-    testaccuracy=accuracy_score(Y_test,testpreds)
-    trainaccuracy=accuracy_score(Y_train,trainpreds)
-    f1score= f1_score(Y_train,trainpreds, average=None)
-    modelgeneration(classifier)
-    return testaccuracy,trainaccuracy,f1score
-def modelgeneration(classifier):
-    modelspath='./static/ML Model'
-    modelfile= "diabetespredmodelusingxgboost.json"
-    # path_to_train="C:\\Users\\VICKFURY\\Documents\\projects\\Python Scripts\\ml\\ml codes\\supervised\\INTRODUCTION TO TENSORFLOW FOR COMPUTER VISION\\traffic signs\\Datasets\\Model Training Data\\Train"
-    if not os.path.isdir(modelspath):
-        os.makedirs(modelspath)
-    modelfile=os.path.join(modelspath,modelfile)
-    if not os.path.exists(modelfile):
-        classifier.save_model(modelfile)
-    else:
-        os.remove(modelfile)
-        classifier.save_model(modelfile)
+import sys
+sys.setrecursionlimit(10000)
 
-    return True
-def predict():
-    # filename="diabetespredmodelusingxgboost.json"
-    xgb_cls=xgb.XGBClassifier(learning_rate =0.0001,
-    n_estimators=1000,
-    max_depth=5,
-    min_child_weight=1,
-    gamma=0,
-    subsample=0.8,
-    colsample_bytree=0.8,
-    objective= 'binary:logistic',
-    nthread=4,
-    scale_pos_weight=1,
-    seed=27)
-    xgb_cls.load_model(modelpathfile)
-        # pregnanciesno=request.form["pregnanciesno"]
-        # glucose=request.form["glucose"]
-        # bmi=request.form["bmi"]
-        # insulin=request.form["insulin"]
-        # Age=request.form["Age"]
-        # bloodpressure=request.form["bloodpressure"]
-        # outcome=request.form["outcome"]
-        # DiabetesPedigreeFunction=request.form["DiabetesPedigreeFunction"]
-        # SkinThickness=request.form["SkinThickness"]
-    features=[float(x) for x in request.form.values()]
-    finalfeatures=[np.array(features)]
-    sample1=[6,148,72,35,0,33.6,0.627,50]
-    #sample1=[5,116,74,0,0,25.6,0.201,30]
-    sample1=np.array(sample1)
-    sample1=sample1.reshape(1,-1)
-    prediction=xgb_cls.predict(sample1)
-    return prediction
-def computemetrics(X,Y):
-    X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2,stratify=Y,random_state=42)
+class ModelUtils:
+    def datapreprocessing(diabetesdataurl):
+        diabetesdata= pd.read_csv(diabetesdataurl)
+        diabetesdata[['Glucose','BloodPressure','SkinThickness','Insulin','BMI']] = diabetesdata[['Glucose','BloodPressure','SkinThickness','Insulin','BMI']].replace(0,np.NaN)
+        diabetesdata.dropna(inplace=True)
+        X=diabetesdata.drop(['Outcome','BloodPressure'],axis=1)
+        Y=diabetesdata['Outcome']
+        # scaler = StandardScaler()
+        # X = scaler.fit_transform(X)
+        return X,Y
+    def train(X,Y):  
+        X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2,stratify=Y,random_state=42)
+        classifier=svm.SVC(kernel='linear')
+        classifier.fit(X_train,Y_train)
+        trainpreds=classifier.predict(X_train)
+        testpreds=classifier.predict(X_test)
+        testaccuracy=accuracy_score(Y_test,testpreds)*100
+        f1score= f1_score(Y_test,testpreds, average=None)        
+        modelspath='./static/ML Model'
+        modelfile= "diabetespredmodelusingxgboost.pkl"
+        modelpathfile=os.path.join(modelspath,modelfile)
+        if not os.path.isdir(modelspath):
+            os.makedirs(modelspath)
+        modelpathfile=os.path.join(modelspath,modelfile)
+        if not os.path.exists(modelpathfile):
+            pk.dump(classifier,open(modelpathfile,'wb'))
+        else:
+            os.remove(modelpathfile)
+            pk.dump(classifier,open(modelpathfile,'wb'))
 
-    modelpipe1=Pipeline([
-        ('model', LogisticRegression()),
-    ])
-    modelpipe2=Pipeline([
-        
-        ('model',xgb.XGBClassifier(learning_rate =0.0001, n_estimators=1000,max_depth=5,min_child_weight=1,gamma=0,subsample=0.8,colsample_bytree=0.8,objective= 'binary:logistic', nthread=4,scale_pos_weight=1,seed=27))
+        return testaccuracy,f1score
+ 
+    def computemetrics(X,Y):
+        X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2,stratify=Y,random_state=42)
 
-    ])
-    modelpipe3=Pipeline([
-        
-        ('model', svm.SVC(kernel='linear')),
-    ])
-    modelpipe4=Pipeline([
-        
-        ('model', RandomForestClassifier(n_estimators=2,criterion="entropy")),
-    ])
-    modelpipe5=Pipeline([
-        
-        ('model', KNeighborsClassifier(n_neighbors=2,metric="minkowski",p=2)),
-    ])
+        modelpipe1=Pipeline([
+            ('model', LogisticRegression()),
+        ])
+        modelpipe3=Pipeline([
+            
+            ('model', svm.SVC(kernel='linear')),
+        ])
+        modelpipe4=Pipeline([
+            
+            ('model', RandomForestClassifier(n_estimators=2,criterion="entropy")),
+        ])
+        modelpipe5=Pipeline([
+            
+            ('model', KNeighborsClassifier(n_neighbors=2,metric="minkowski",p=2)),
+        ])
 
 
-    modelpipe1.fit(X_train, Y_train)
-    modelpipe2.fit(X_train, Y_train)
-    modelpipe3.fit(X_train, Y_train)
-    modelpipe4.fit(X_train, Y_train)
-    modelpipe5.fit(X_train, Y_train)
+        modelpipe1.fit(X_train, Y_train)
+        # modelpipe2.fit(X_train, Y_train)
+        modelpipe3.fit(X_train, Y_train)
+        modelpipe4.fit(X_train, Y_train)
+        modelpipe5.fit(X_train, Y_train)
+        testpred1=modelpipe1.predict(X_test)
+        # testpred2=modelpipe2.predict(X_test)
+        testpred3=modelpipe3.predict(X_test)
+        testpred4=modelpipe4.predict(X_test)
+        testpred5=modelpipe5.predict(X_test)
+        accuracy={
+                'Logistic Regression':accuracy_score(Y_test,testpred1)*100,
+                'SVM':accuracy_score(Y_test,testpred1)*100,
+                'Random Forest':accuracy_score(Y_test,testpred1)*100,        
+                'K-Nearest Neighbours':accuracy_score(Y_test,testpred1)*100
+                # 'confusion matrix ':confusion_matrix(Y_test,testpred1),
+                # 'classification report':classification_report(Y_test,testpred1)
+            },
+        mse={
+                'Logistic_Regression': mean_squared_error(Y_test,testpred1),
+                'SVM': mean_squared_error(Y_test,testpred3),
+                'Random Forest': mean_squared_error(Y_test,testpred4),
+                'K-Nearest Neighbours': mean_squared_error(Y_test,testpred5),
+            },
+        fs={
+                'Logistic Regression': f1_score(Y_test, testpred1),
+                'SVM': f1_score(Y_test, testpred3),
+                'Random Forest': f1_score(Y_test, testpred4),
+                'K-Nearest Neighbours':f1_score(Y_test, testpred5),
+            }
+            
+        return accuracy,mse,fs
 
-    trainpred1=modelpipe1.predict(X_train)
-    trainpred2=modelpipe2.predict(X_train)
-    trainpred3=modelpipe3.predict(X_train)
-    trainpred4=modelpipe4.predict(X_train)
-    trainpred5=modelpipe5.predict(X_train)
-
-    testpred1=modelpipe1.predict(X_test)
-    testpred2=modelpipe2.predict(X_test)
-    testpred3=modelpipe3.predict(X_test)
-    testpred4=modelpipe4.predict(X_test)
-    testpred5=modelpipe5.predict(X_test)
-    
-    # trainaccuracy=[accuracy_score(Y_train,trainpred1),accuracy_score(Y_train,trainpred2),accuracy_score(Y_train,trainpred3),accuracy_score(Y_train,trainpred4),accuracy_score(Y_train,trainpred5)]
-    # testaccuracy=[accuracy_score(Y_test,testpred1),accuracy_score(Y_test,testpred2),accuracy_score(Y_test,testpred3),accuracy_score(Y_test,testpred4),accuracy_score(Y_test,testpred5)]
-    # print (display_classificationreport(Y_test, testpred1))
-    # print (display_classificationreport(Y_test, testpred2))
-    # print (display_classificationreport(Y_test, testpred3))
-    # print (display_classificationreport(Y_test, testpred4))
-    # print (display_classificationreport(Y_test, testpred5))
-
-    # print(f"Logistic Regression Mean Squared Error : {calculate_meansquarederror(modelpipe1, Y_train, trainpred1):.4f}")
-    # print(f"XGBoost  Mean Squared Error : {calculate_meansquarederror(modelpipe2, Y_train, trainpred2):.4f}")
-    # print(f"SVM  Mean Squared Error: {calculate_meansquarederror(modelpipe3, Y_train, trainpred3):.4f}")
-    # print(f"Random Forest  Mean Squared Error: {calculate_meansquarederror(modelpipe4, Y_train, trainpred4):.4f}")
-    # print(f"KNN Train Mean Squared Error: {calculate_meansquarederror(modelpipe5, Y_train, trainpred5):.4f}")
-
-    # print(f"Logistic Regression Mean Squared Error : {calculate_meansquarederror(modelpipe1, Y_test, testpred1):.4f}")
-    # print(f"XGBoost  Mean Squared Error : {calculate_meansquarederror(modelpipe2, Y_test, testpred2):.4f}")
-    # print(f"SVM  Mean Squared Error: {calculate_meansquarederror(modelpipe3, Y_test, testpred3):.4f}")
-    # print(f"Random Forest  Mean Squared Error: {calculate_meansquarederror(modelpipe4, Y_test, testpred4):.4f}")
-    # print(f"KNN Train Mean Squared Error: {calculate_meansquarederror(modelpipe5, Y_test, testpred5):.4f}")
-    return  calculate_model_metrics(modelpipe2,Y_test, testpred1),calculate_model_metrics(modelpipe2,Y_test, testpred2),calculate_model_metrics(modelpipe3,Y_test, testpred3),calculate_model_metrics(modelpipe4,Y_test, testpred4),calculate_model_metrics(modelpipe5,Y_test, testpred5)
-def calculate_model_metrics(algorithm,true, pred):
-    algorithm_details={f'{algorithm}': 
-    {
-    f'accuracy of {algorithm}':accuracy_score(true,pred),        f'mean squared error of{algorithm}': mean_squared_error(true,pred),
-    f'confusion matrix of {algorithm}':confusion_matrix(true,pred),
-    f'f1 score of {algorithm}':f1_score(true, pred),
-    f'classification report of {algorithm}':classification_report(true,pred)
-    }
-    }
-    return algorithm_details
 
     
